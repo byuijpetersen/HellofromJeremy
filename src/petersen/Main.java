16e15196679cf445021d1ed74d10c771a9d4da4c
abrower10612 <abrower7@byui.edu>
2021-01-31 00:13:15 -0700

    Saying hello to Jeremy for Prove04

--- src/petersen/Main.java
+++ src/petersen/Main.java
@@ -3,7 +3,8 @@ package petersen;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello, my name is Jeremy");
-	System.out.println("Hello Jeremy, my name is Jennifer.");
+		System.out.println("Hello, my name is Jeremy");
+		System.out.println("Hello Jeremy, my name is Jennifer.");
+		System.out.println("Hello Jeremy, my name is Andrew.");
     }
 }
